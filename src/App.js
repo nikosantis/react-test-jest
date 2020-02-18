93d9b32f4e1512053bdc08d945cf9457dd990fec
Nikolas Santis <nikosantis@gmail.com>
2020-02-18 20:36:43 -0300

    testing components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react'
 
+export const Title = () => (
+  <h1>Introduccion a Unit Testing</h1>
+)
+
 const App = () => {
   return (
-    <div>
-      <h1>
-        Introduccion a Unit Testing
-      </h1>
-    </div>
+    <section id='main'>
+      <Title />
+      <div className='container'>
+        <span num={3} active={false}>Primero</span>
+        <span num='3' active='false'>Segundo</span>
+      </div>
+      <input type='text'/>
+      <input type='checkbox'/>
+    </section>
   )
 }
 
